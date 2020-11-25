3b5f0db33cb504d7a92480c15c25210bc5f03569
John Ellis <34400721+johnellis0@users.noreply.github.com>
2020-11-25 17:47:13 +0000

    Add Setting object

---
+++ gs-sheets-menu.js
@@ -0,0 +1,23 @@
+function Setting(name, defaultValue, description, settingType){
+    this.name = name;
+    this.defaultValue = name;
+    this.description = description;
+    this.settingType = settingType;
+}
+Setting.prototype = {
+    _value: undefined,
+
+    getDefault: function(){
+        return this.defaultValue;
+    },
+};
+
+/**
+ * Enum for settings types. Used to determine how to display on menu / validate etc.
+ * @readonly
+ * @enum {number}
+ */
+const SettingTypes = Object.freeze({
+    TEXT: 1,
+    BOOL: 2,
+});
